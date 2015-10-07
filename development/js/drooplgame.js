dd20a8efce34df04684c15f2a0c747aaa2d9c257
Rachouan <rachouan_15@hotmail.fr>
2015-10-07 15:21:56 +0200

    update

--- development/js/drooplgame.js
+++ development/js/drooplgame.js
@@ -5,52 +5,91 @@ $(function () {
     }
 
 	function startGame () {
+		
+		var coins = [];
+		var canvas = document.getElementById("cnvs");
+		canvas.width = $(canvas).parent().width();
+		canvas.height = $(canvas).parent().height();
+		var context = canvas.getContext("2d");
+		var coinsprite = [document.getElementById("coin"), document.getElementById("coin2"), document.getElementById("coin3")];
+
+		var xpos = $(canvas).parent().width()/2;
+		var speed = $(canvas).width()/20;
+
+		$(window).on("keydown",function(e){
+
+		   console.log(e.keyCode);
+
+		  switch(e.keyCode){
+		    case 37:
+		    xpos -= speed;
+		    break;
+
+		    case 39:
+		    xpos += speed;
+		    break;
+
+		    case 40:
+		    jump ++;
+		    break;
+		  }
+
+		});
+
+		setInterval(function () {
 
 
-	
-	var canvas = document.getElementById("cnvs");
-	canvas.width = $(canvas).parent().width();
-	canvas.height = $(canvas).parent().height();
-	var context = canvas.getContext("2d");
+		  if(xpos <= 0){
+		    xpos = 0;
+		  }else if(xpos >= canvas.width){
+		    xpos = canvas.width-40;
+		  }
 
-	var xpos = $(canvas).parent().width()/2;
-	var speed = $(canvas).width()/20;
+		  context.clearRect(0, 0, canvas.width, canvas.height);
 
-	$(window).on("keydown",function(e){
+		  for (var i = coins.length - 1; i >= 0; i--) {
+		  	var coin = coins[i];
+		  	if(coin.yPos < $(canvas).height()){
 
-	   console.log(e.keyCode);
+		  		coin.count++;
+		  		if(coin.count == 39){
+		  			coin.count = 0;
+		  		}
 
-	  switch(e.keyCode){
-	    case 37:
-	    xpos -= speed;
-	    break;
+		  		var index = parseInt(coin.count/10);
 
-	    case 39:
-	    xpos += speed;
-	    break;
+		  		console.log(index)
 
-	    case 40:
-	    jump ++;
-	    break;
-	  }
+		  		var url = "coin"+index;
 
-	});
+		  		var coinImg = document.getElementById(url);
+		  		context.drawImage(coinImg,coin.xPos,coin.yPos);
+			  	coin.yPos += coin.speed;
+		  	}else{
 
-	setInterval(function () {
+		  		coins.splice(coins.indexOf(coin),1);
+		  	}
+		  	
+
+		  };
+
+		  context.fillStyle = "#FF7F66";
+		  context.fillRect(xpos,canvas.height - 40,40,40);
+		});
+
+		setInterval(function () {
+
+			coins.push(createCoint(0,0,1,0,0));
 
 
-	  if(xpos <= 0){
-	    xpos = 0;
-	  }else if(xpos >= canvas.width){
-	    xpos = canvas.width-40;
-	  }
+		  console.log(coins);
 
-	  context.clearRect(0, 0, canvas.width, canvas.height);
+		},1000);
 
-	  context.fillStyle = "#FF7F66";
-	  context.fillRect(xpos,canvas.height - 200,40,40);
-	});
+	}
 
+	function createCoint (xPos,yPos,speed,frame,count) {
+		return { xPos:xPos, yPos:yPos,speed:speed,frame:frame,count:count}
 	}
 	
 });
--- development/pages/notfound/notFound.php
+++ development/pages/notfound/notFound.php
@@ -3,4 +3,8 @@
 	<canvas id="cnvs">
 		
 	</canvas>
+	<img src="images/assets/game/coin.png" class="hide" id="coin0">
+	<img src="images/assets/game/coin2.png" class="hide" id="coin1">
+	<img src="images/assets/game/coin3.png" class="hide" id="coin2">
+	<img src="images/assets/game/coin4.png" class="hide" id="coin3">
 </section>
