943a3fbcc44dfa1e0e388489a4839bf5238d3b80
borisdebusscher <borisdebu@gmail.com>
2015-10-12 16:14:48 +0200

    update

--- development/pages/user/register.php
+++ development/pages/user/register.php
@@ -1,20 +1,39 @@
 <article class="register">
     
-    <div class="container">
+    <div class="register-box">
         
-        <section class="step_1">
-            <aside class="left">
-            </aside>
-            <aside class="right">
-            </aside>
-        </section>
-        <section class="step_1">
-            <aside class="left">
-            </aside>
-            <aside class="right">
-            </aside>
-        </section>
+        <div class="container">
         
+            <section class="step_1">
+                <aside class="left">
+                    step1
+                </aside>
+                <aside class="right">
+                </aside>
+            </section>
+            <section class="step_2">
+                <aside class="left">
+                    step2
+                </aside>
+                <aside class="right">
+                </aside>
+            </section>
+            <section class="step_3">
+                <aside class="left">
+                </aside>
+                <aside class="right">
+                </aside>
+            </section>
+        
+        </div>
+        
+        <nav class="pages">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
         
     </div>
     
