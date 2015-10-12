fad2ec552aae120d281bb9aa2907719f76aabe6d
borisdebusscher <borisdebu@gmail.com>
2015-10-12 15:17:51 +0200

    update

---
+++ development/pages/user/register.php
@@ -0,0 +1,21 @@
+<article class="register">
+    
+    <div class="container">
+        
+        <section class="step_1">
+            <aside class="left">
+            </aside>
+            <aside class="right">
+            </aside>
+        </section>
+        <section class="step_1">
+            <aside class="left">
+            </aside>
+            <aside class="right">
+            </aside>
+        </section>
+        
+        
+    </div>
+    
+</article>
