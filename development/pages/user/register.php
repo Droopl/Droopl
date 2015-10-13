8468ca17c0a8c9630ae1d5e2506f6da7b85bcfe5
borisdebusscher <borisdebu@gmail.com>
2015-10-13 19:44:56 +0200

    update

--- development/pages/user/register.php
+++ development/pages/user/register.php
@@ -6,10 +6,29 @@
         
             <section class="step_1">
                 <aside class="left">
-                    <form>
+                    <h1>Registration</h1>
+                    <form id="registration_form_step_1" name="registration_form_step_1" method="post">
+                        <input type="text" id="first" name="first" placeholder="Firstname" required>
+                        <label for="first">*</label>
+                        <input type="text" id="last" name="last" placeholder="Last name" required>
+                        <label for="last">*</label>
+                        <input type="email" id="mail" name="mail" placeholder="Email" required>
+                        <label for="mail">*</label>
+                        <input type="password" id="pass" name="pass" placeholder="Password" required>
+                        <label for="pass">*</label>
+                        <input type="password" id="repeat_pass" name="repeat_pass" placeholder="Repeat password" required>
+                        <label for="repeat_pass">*</label>
+                        <label for="select_language">Language: </label>
+                        <select id="select_language">
+                            <option value="en">English</option>
+                            <option value="nl">Nederlands</option>
+                            <option value="fr">Français</option>
+                        </select>
+                        <input type="submit" id="submit_step_1" name="submit_step_1" value="Step 2">
                     </form>
                 </aside>
                 <aside class="right">
+                    <img src="images/assets/emty_data/empty_collection.svg">
                 </aside>
             </section>
             <section class="step_2">
