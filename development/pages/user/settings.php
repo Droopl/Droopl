e2508f1280066ee88c148f0d00ca16b69b3dc96e
borisdebusscher <borisdebu@gmail.com>
2015-11-12 19:46:12 +0100

    update

--- development/pages/user/settings.php
+++ development/pages/user/settings.php
@@ -1,1 +1,24 @@
-settings.php
+<article class="settings">
+    <div class="feed">
+        <section class="settings-container">
+            <aside class="left">
+                <form id="upload-profile-img">
+                    <header id="upload">
+                        <div class="dragndrop" id="dragndrop">
+                            <div class="preloader">
+                                <ul class="progress">
+                                </ul>
+                                <span class="remove-file"><p class="icon-cross"></p></span>
+                                <input type="file" id="collection_image" name="collection_image" accept="image/*">
+                            </div>
+                        </div>
+                        <h1>Drag and drop</h1>
+                        <p>Or press to browse</p>
+                    </header>
+                </form>
+            </aside>
+            <aside class="right">
+            </aside>
+        </section>
+    </div>
+</article>
