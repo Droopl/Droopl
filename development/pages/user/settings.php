ed19de7923d84fe6acb3699d9ffa06fed62bc7ba
borisdebusscher <borisdebu@gmail.com>
2015-11-14 15:49:53 +0100

    update

--- development/pages/user/settings.php
+++ development/pages/user/settings.php
@@ -18,16 +18,16 @@
             <aside class="right">
                 <h1 class="settings-title">Account Settings</h1>
                 <label for="first">Firstname:</label>
-                <input type="text" id="first" name="first">
+                <input type="text" id="first" name="first" value="<?php echo $user['firstname']; ?>">
                 <label for="last">Last name:</label>
-                <input type="text" id="last" name="last">
-                <label for="new_pass">Password:</label>
-                <input type="password" id="new_pass" name="new_pass">
+                <input type="text" id="last" name="last" value="<?php echo $user['lastname']; ?>">
+                <label for="new_pass">New password:</label>
+                <input type="password" id="new_pass" name="new_pass" placeholder="">
                 <label for="repeat_new_pass">Repeat password:</label>
-                <input type="password" id="repeat_new_pass" name="repeat_new_pass">
+                <input type="password" id="repeat_new_pass" name="repeat_new_pass" placeholder="">
                 <div class="select-language">
                                 <h1>Language: </h1>
-                                <div class="flag en"></div>
+                                <div class="flag <?php echo $user['lang'] ?>"></div>
                                 <ul class="lang-list">
                                     <li class="en"><span class="en-flag"></span>English</li>
                                     <li class="nl"><span class="nl-flag"></span>Nederlands</li>
