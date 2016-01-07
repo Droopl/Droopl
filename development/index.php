2ddf02a7161ed36316c9ad1333b2af4089c57347
Rachouan <rachouan_15@hotmail.fr>
2016-01-08 00:56:12 +0100

    update

--- development/index.php
+++ development/index.php
@@ -4,6 +4,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
+date_default_timezone_set('Europe/Brussels');
+
 session_start();
 
 define('DS',DIRECTORY_SEPARATOR);
