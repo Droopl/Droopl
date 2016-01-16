ac96601b2f17f4e0bc5f336b5c6fc8ade507a780
Rachouan <rachouan_15@hotmail.fr>
2016-01-16 12:21:04 +0100

    update

--- development/index.php
+++ development/index.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
+
+date_default_timezone_set('America/Phoenix');
 
 define('DS',DIRECTORY_SEPARATOR);
 define('WWW_ROOT', dirname(__FILE__).DS);
